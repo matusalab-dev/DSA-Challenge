867d0d870e564e6160074f1e4c517819cd2336a0
Matusala Abrha <mtslabrh97@gmail.com>
2025-06-25 23:11:56 +0300

    solve the veryBigSum Hackerrank Challenge

--- HackerRankWarmUp/aVeryBigSum/script.js
+++ HackerRankWarmUp/aVeryBigSum/script.js
@@ -4,12 +4,12 @@
  * The function is expected to return a LONG_INTEGER.
  * The function accepts LONG_INTEGER_ARRAY ar as parameter.
  */
-
 function aVeryBigSum(ar) {
-  // Write your code here
-  let sum = 0;
-  for (let i = 0; i < ar.length; i++) {
-    sum += ar[i];
-  }
-  return sum;
+    let sum = 0;
+    
+    for(let i = 0; i < ar.length; i++) {
+    sum += ar[i]
+    }
+    // use the BigInt data type to handle long set of integers
+    return BigInt(sum)
 }
